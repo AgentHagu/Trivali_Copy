f27af11fdcae2ee546ecf5d73d47dfb78d765448
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-05-22 18:36:34 +0800

    vercel.json trial

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 var fs = require("fs");
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/HTML' });
     console.log("HELLO");
-    fs.writeFile('hello.txt', "TEST WORKED WOWOWOWOW", err => {});
-    res.end('Hello World!');
+    fs.writeFile('hello.txt', "TEST WORKED WOWOWOWOW", err => { });
+    res.end('ONE TWO THREE FOUR!');
 }).listen(8080); 
