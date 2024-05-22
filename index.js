425efbfa26e9169b84b2df399689d24a91689094
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-05-22 18:21:47 +0800

    index.js testing

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var fs = require("fs");
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    console.log("HELLO");
+    fs.writeFile('hello.txt', "TEST WORKED WOWOWOWOW", err => {});
     res.end('Hello World!');
 }).listen(8080); 
